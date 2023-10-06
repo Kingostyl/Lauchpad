22b640628467d496317c1699c0e992ffc4dcf786
Kingostyl <emr8770@gmail.com>
2023-10-06 13:05:53 +0700

    add

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,12 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Macos Lauchpad',
   description: 'By MacOS From Apple.inc',
+  icons: {
+    icon: ['/favicon.ico?v=4'],
+    apple: ['/apple-touch-icon.png?v=4'],
+    shortcut: ['/apple-touch-icon.png'],
+  },
+  manifest: '/site.webmanifest',
 }
 
 export default function RootLayout({
